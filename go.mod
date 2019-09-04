2d11e69506b01dfb3a6b27d4d28156b0eb3d5656
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2019-09-03 22:15:15 -0700

    Add go.mod, update travis, be even more explicit about irchandle's experimental nature

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/jakebailey/irc
+
+go 1.11
+
+require github.com/stretchr/testify v1.4.0
--- irchandle/irchandle.go
+++ irchandle/irchandle.go
@@ -1,3 +1,3 @@
 // Package irchandle implements net/http-like handler support.
-// This is an experiment, and may not stick around.
+// This is an experiment, and may not stick around. Do not depend on it.
 package irchandle
